9ae3843d3012f6109a1f51e5c6b7572133c11166
Santosh Jadhav <santoshjadhav@Santoshs-MacBook-Pro.local>
2019-05-18 00:27:44 +0530

    Added code

--- lib/chef-umami/helpers/inspec.rb
+++ lib/chef-umami/helpers/inspec.rb
@@ -82,7 +82,7 @@ module Umami
         command = resource.command.gsub(/\/opt\/ruby-2.3.5\/bin\/ruby/, "/opt/chef/embedded/bin/ruby")
         if resource.name =='Coupa Chef Client'  
           test = ["describe crontab('#{resource.user}') do"]
-          test << "its('commands') { should include \"#{command}\""}"
+          test << "its('commands') { should include \"#{command}\"}"
         else
           test = ["describe crontab('#{resource.user}').commands(\"#{command}\") do"] 
           test << "its('minutes') { should cmp '#{resource.minute}' }"
