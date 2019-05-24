40ab47a449f5c3d43f85e3777513ac013f71360e
Santosh Jadhav <santoshjadhav@Santoshs-MacBook-Pro.local>
2019-05-24 17:09:52 +0530

    Added 10 min to gen test cases

--- lib/chef-umami/options.rb
+++ lib/chef-umami/options.rb
@@ -24,7 +24,7 @@ module Umami
       options[:policyfile] = 'Policyfile.rb'
       options[:test_root] = 'spec'
       options[:unit_tests] = true
-
+      options[:cookbooks_dir] = '/var/chef/cache/cookbooks/coupa-*'
       parser = OptionParser.new do |opts|
         opts.banner = opts.banner + "\n\nA taste you won't forget!\n\n"
         opts.on('-h', '--help', 'Prints this help message') {
@@ -43,6 +43,7 @@ module Umami
                 "Specify one or more recipes for which we'll write tests" \
                 ' (DEFAULT: All recipes)') do |recipes|
           options[:recipes] = recipes
+          options[:all_supported_cookbooks] = []
           if options[:recipes][0] == 'all'
              options[:recipes] = get_all_recipies_list
              puts " Generating Unit and Integration tests on following recipies"
@@ -59,15 +60,23 @@ module Umami
         end
 
         opts.on('-n', '--name-recipe NAMERECIPE', 'Specify name recpie' \
-          " (DEFAULT: #{options[:name_recipes]})") do |name_recipe|
+                " (DEFAULT: #{options[:name_recipes]})") do |name_recipe|
           options[:name_recipes] = name_recipe
           options[:recipes] = name_recipe
         end
-        #opts.on('-c', '--config-json JSON_CONFIG_PATH','Json config file' \
-        #        " (DEFAULT: #{options[:json_config]})") do |json_config|
-        #  options[:json_config] = json_config
-        #  FileUtils.cp("#{options[:json_config]}","/tmp/config.json")
-        #end
+
+
+        opts.on('-a', '--all-supported-cookbooks SUPPORTED_COOKBOOKS', 'Provide true and false' \
+                " (DEFAULT: #{options[:all_supported_cookbooks]})") do |supported_cookbooks|
+          options[:all_supported_cookbooks] = supported_cookbooks
+           options[:recipes] = []
+        end
+
+        opts.on('-c', '--cookbook-dir COOKBOOKS_DIR', 'Cookbook dir to execute' \
+                " (DEFAULT: #{options[:cookbooks_dir]})") do |dir|
+          options[:cookbooks_dir] = dir
+        end
+
         opts.on('-v', '--version', 'Show version and exit') {
           puts "chef-umami v#{Umami::VERSION}"
           exit
@@ -79,7 +88,7 @@ module Umami
         puts "Warning: #{e.message}"
         if e.message =~ /--pattern/
           puts 'Ah, this is likely parsed from `rspec` options. We can safely ignore this.'
-        end
+      end
         puts 'Ignoring the option.'
       end
       options
@@ -93,6 +102,6 @@ module Umami
         recpies_list.push(recip)
       end
       return recpies_list
-    end 
+    end
   end
 end
